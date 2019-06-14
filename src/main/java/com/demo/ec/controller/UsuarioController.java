9b2f63f86393bdcc76ccb9c36ab09f4a67164960
KRUGER\khidalgo <khidalgo@kruger.com.ec>
2019-06-14 16:33:14 -0500

    save controller usuario

--- src/main/java/com/demo/ec/controller/UsuarioController.java
+++ src/main/java/com/demo/ec/controller/UsuarioController.java
@@ -1,17 +1,22 @@
 package com.demo.ec.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.ec.model.Usuario;
+import com.demo.ec.service.IUsuarioService;
 
 @RestController
 @RequestMapping(value = "/usuario")
 public class UsuarioController {
-	
+
 	@Autowired
-	
+	private IUsuarioService usuarioService;
+
 	@RequestMapping(value = "/saveUsuario")
-	public Usuario saveUuario (Usuario usuario) {
-		
+	public Usuario saveUsuario(@RequestBody() Usuario usuario) {
+		return this.usuarioService.save(usuario);
 	}
 }
